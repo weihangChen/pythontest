2429ef1451c6450b0e51bffb56671919e19695da
weihang <weihang@MC>
2017-07-19 01:07:08 +0200

    refac

--- PythonTest/marko/markoservice.py
+++ PythonTest/marko/markoservice.py
@@ -14,9 +14,9 @@ class Prob:
 
 
 import unittest
-class StationaryDistributionTest(unittest.TestCase):
+class StepByStepTest(unittest.TestCase):
     #https://www.youtube.com/watch?v=5uGhAQOs6Aw&index=5&list=PLKG3ExuC02lsnZUJDdOlYJd5CRe3otzq1
-    def test_dtationarydistribution1(self):
+    def test_ab1(self):
         state_A = State("A")
         state_B = State("B")
 
@@ -36,7 +36,7 @@ class StationaryDistributionTest(unittest.TestCase):
         self.assertTrue(prob_state_A_step_3 == 0.376)
 
     #https://www.youtube.com/watch?v=5I0YPndroIE&index=7&list=PLKG3ExuC02lsnZUJDdOlYJd5CRe3otzq1
-    def test_dtationarydistribution2(self):
+    def test_ab2(self):
         state_A = State("A")
         state_B = State("B")
 
@@ -57,5 +57,24 @@ class StationaryDistributionTest(unittest.TestCase):
 
 
 
+        #https://www.youtube.com/watch?v=4xfEvRGRBnU&list=PLKG3ExuC02lsnZUJDdOlYJd5CRe3otzq1&index=13
+    def test_sequencetoprob(self):
+        sequence = ["s","s","s","s","s","r","s","s","s","r","r"]
+        state_A = State("A")
+        state_B = State("B")
 
+        prob_state_A_to_state_A = Prob(state_A, state_A, 0.5)
+        prob_state_B_to_state_A = Prob(state_B, state_A, 1)
+
+       
+        #test state_A at step2
+        prob_state_A_step_1 = 0.5
+        prob_state_B_step_1 = 1- prob_state_A_step_1
+        prob_state_A_step_2 = prob_state_A_to_state_A.prob * prob_state_A_step_1 + prob_state_B_to_state_A.prob * prob_state_B_step_1
+        self.assertTrue(prob_state_A_step_2 == 0.75)
+
+        #test state_A at step3
+        prob_state_B_step_2 = 1 - prob_state_A_step_2
+        prob_state_A_step_3 = prob_state_A_to_state_A.prob * prob_state_A_step_2 + prob_state_B_to_state_A.prob * prob_state_B_step_2
+        self.assertTrue(prob_state_A_step_3 == 0.625)
 
