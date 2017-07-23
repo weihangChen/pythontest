5eff0a8e507a24f5259b24698e6eb38cc2a43c78
weihang <weihang@MC>
2017-07-23 14:44:54 +0200

    refac

--- PythonTest/search/informedsearchservice.py
+++ PythonTest/search/informedsearchservice.py
@@ -2,7 +2,7 @@ import numpy as np
 #we model something totally different than from "searchservice.py"
 #we are not navigating through a map tyring to find the detination
 class Node():
-    def __init__(self, name : str, probability : int):
+    def __init__(self, name : str, probability):
         self.name = name
         self.probability = probability
 
@@ -27,9 +27,16 @@ class Path():
 #[sab],[sad], rest are removed, the logic here is totally differen than
 #the one from super class, we will expand
 class BeamSearch():
-    def __init__(self, beam_size,  process_data):       
+    #letter_matrix is two dimentional matrix
+    #x represents letter position, y represents the cadidate letters with prob
+    def __init__(self, beam_size,  letter_matrix):       
         self.beam_size = beam_size
-        self.process_data = process_data
+        #sort each column's prob from high to low
+        self.letter_matrix = letter_matrix
+        self.pathes = []
+        for column_data in self.letter_matrix:
+             column_data = column_data.sort(key=lambda x: x.probability, reverse=True)
+        
 
 
     def appendNodeAndGetNewPath(self, old_path, new_node):
@@ -39,27 +46,47 @@ class BeamSearch():
         return new_path
    
     def run(self):
-        for data in self.process_data:
-            
-            #sort the data and take top-n
-            data = data.sort(key=lambda x: x.probability, reverse=True)
-            data = data[:beam_size]
+        #init path with nodes from first column
+        
+        for node in self.letter_matrix[0][:self.beam_size]:
+            path = Path()
+            path.nodes.append(node)
+            path.total_prob = node.probability
+            self.pathes.append(path)
+
+        for column_letter_candidates in self.letter_matrix[1:-1]:
+            new_pathes = []
             
             for path in self.pathes:
-                for x in data:
-                    new_path = self.appendNodeAndGetNewPath(path, x)
+                for node in column_letter_candidates:
+                    new_path = self.appendNodeAndGetNewPath(path, node)
+                    new_path.total_prob = path.total_prob + node.probability
                     new_pathes.append(new_path)
-                #only keep the beam-size amount of pathes
-                new_pathes.sort(key=lambda x: x.total_prob, reverse=True)
-                new_pathes = new_pathes[:beam_size]
+            #only keep the beam-size amount of pathes
+            new_pathes.sort(key=lambda x: x.total_prob, reverse=True)
+            self.pathes = new_pathes[:self.beam_size]
            
-            self.pathes = new_pathes
 
 import unittest
 class BeamSearchTest(unittest.TestCase):
     def test_beam(self):
-        data = []
-        service = BeamSearch(2,data)
+        dic = ["cat","dog","ape"]
+        #column1
+        n_x1_y1 = Node("c", 0.6)
+        n_x1_y2 = Node("d", 0.8)
+        n_x1_y3 = Node("a", 0.7)
+        #column2
+        n_x2_y1 = Node("a", 0.9)
+        n_x2_y2 = Node("o", 0.6)
+        n_x2_y3 = Node("p", 0.7)
+        #column3
+        n_x3_y1 = Node("t", 0.9)
+        n_x3_y2 = Node("g", 0.8)
+        n_x3_y3 = Node("e", 0.7)
+        
+        letter_matrix = [[n_x1_y1,n_x1_y2,n_x1_y3],[n_x2_y1,n_x2_y2,n_x2_y3],[n_x3_y1,n_x3_y2,n_x3_y3]]
+        service = BeamSearch(2,letter_matrix)
         service.run()
+        print(service.pathes)
         #path_str = service.complete_path.getPathStr()
         #self.assertTrue(path_str == "s->a->d->g")
